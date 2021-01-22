3d5e90c6bbb212748b2ad93e2f35c8993d80b56f
tiago <tiagolemespalhano@gmail.com>
2021-01-21 21:54:19 -0300

    Menu page is already made

--- includes/arrays.php
+++ includes/arrays.php
@@ -54,4 +54,71 @@
 	);
 
 
+
+
+		//team members
+	$menu_items = array(
+
+
+
+
+
+		"club-sandwich" => array(
+			
+			"title" => "Club Sandwich",
+			"price" => 11,
+			"blurb" => "Bacon Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+			"drink" => "CLub Soda"
+
+		),
+
+
+
+
+
+
+
+		"dill-salmon" => array(
+			
+			"title" => "Lemon &amp; Dill Salmon",
+			"price" => 18,
+			"blurb" => "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+			"drink" => "Fancy Wine"
+
+		),
+
+
+
+
+
+
+		"super-salad" => array(
+			
+			"title" => "The Super Salad<sup>&reg;</sup> ",
+			"price" => 34,
+			"blurb" => "Excepteur sint occaecat cupidatat non proident.",
+			"drink" => "Jug o' Water"
+
+		),
+
+
+
+
+
+
+
+		"mexican-barbacoa" => array(
+			
+			"title" => "Mexican Barbacoa",
+			"price" => 23,
+			"blurb" => "Sunt in culpa qui officia deserunt mollit anim id est laborum.",
+			"drink" => "Beer with a Lime"
+
+		)
+
+
+	);
+
+
 ?>
+
--- menu.php
+++ menu.php
@@ -1,7 +1,41 @@
+<?php define("TITLE", "Menu | Franklin's Fine Dining"); ?>
+
+<?php require('includes/header.php'); ?>
+
+<div id="menu-items">
+
+	<h1>Our Delicious Menu</h1>
+	<p>Like our team, our menu is very small &mdash; but dang, does it ever pack a punch!</p>
+	<p><em>CLick any menu item to learn more about it.</em></p>
+
+	<hr>
+
+	<ul>
+		<?php 
+			foreach ($menu_items as $dish => $item) {
+			?>
+			<li>
+
+				<a href="dish.php?item=<?= $dish ?>">
+					<?= $item['title'] ?>
+				</a>
+
+				<sup> $</sup>
+				
+				<?= $item['price'] ?>
+				
+			</li>
+			<?php
+			}
+		?>
+	</ul>
+
+</div>
+
 <?php 
+	echo "<pre>";
+	print_r($menu_items);
+	echo "</pre>";
+?>
 
-	define("TITLE", "Menu | Franklin's Fine Dining");
-
-	require('includes/header.php');
-
-	require('includes/footer.php');
+<?php require('includes/footer.php'); ?>
